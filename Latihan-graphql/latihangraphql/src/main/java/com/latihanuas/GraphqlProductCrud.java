247a65b2e212a6c81ce8c30162854ec811fe5d39
jenieeej <162806158+Jenoodle@users.noreply.github.com>
2025-07-05 20:37:39 +0700

    Add files via upload

---
+++ Latihan-graphql/latihangraphql/src/main/java/com/latihanuas/GraphqlProductCrud.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.latihanuas;
+
+import static spark.Spark.*;
+import com.google.gson.*;
+import graphql.*;
+
+public class GraphqlProductCrud {
+
+   public static void main(String[] args) throws Exception {
+       GraphQL graphql = GraphQLConfig.init();
+       Gson gson = new Gson();
+
+       port(4567);
+       post("/graphql", (req, res) -> {
+           res.type("application/json");
+
+           JsonObject request = gson.fromJson(req.body(), JsonObject.class);
+           String query = request.get("query").getAsString();
+
+           ExecutionResult result = graphql.execute(query);
+           return gson.toJson(result.toSpecification());
+       });
+   }
+}
